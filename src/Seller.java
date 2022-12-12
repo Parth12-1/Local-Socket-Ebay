1b627c617e293f971f802fc89a684e5fc98f1aab
Aarush2107 <112562285+Aarush2107@users.noreply.github.com>
2022-12-12 02:27:08 -0500

    Update Seller.java

--- src/Seller.java
+++ src/Seller.java
@@ -1,6 +1,13 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ *Seller.java
+ * This class description is mentioned in the README.md.
+ *
+ * @author Parth Shah, Pranav Konda, Aarush Sachdeva
+ * @version December 10th, 2022
+ */
 public class Seller implements Serializable {
 
     int sellerId;
